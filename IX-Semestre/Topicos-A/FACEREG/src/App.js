1a6bb9cc19f5f5e331c28cc33231c055b46223e6
PhelipeH <phelipe.hass@gmail.com>
2020-04-05 21:17:36 -0300

    Integração react bootstrap

---
+++ IX-Semestre/Topicos-A/FACEREG/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Headers from './Headers';
+import ArticleAddUser from './ArticleAddUser';
+
+function App (props) {
+  return (
+    <div>
+        <Headers />
+        <ArticleAddUser />
+    </div>    
+  );
+};
+
+export default App;
