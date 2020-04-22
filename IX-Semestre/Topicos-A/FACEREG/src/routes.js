6cb5f129fcb6632e5ee5ce0c48189602885eea04
PhelipeH <phelipe.hass@gmail.com>
2020-04-22 20:41:47 -0300

    Atualização integração

--- IX-Semestre/Topicos-A/FACEREG/src/routes.js
+++ IX-Semestre/Topicos-A/FACEREG/src/routes.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/main/main';
-import Register from  './pages/register/register';
+import Create from  './components/Create';
+import Edit from './components/Edit';
+import Show from './components/Show';
+import Detalhe from './components/Detalhes';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Main} />
-            <Route path="/register" component={Register} />
+            <Route path="/create" component={Create} />
+            <Route path="/edit/:id" component={Edit} />
+            <Route path="/show" component={Show} />
+            <Route path="/detalhe/:id" component={Detalhe} />
         </Switch>
     </BrowserRouter>
 );
