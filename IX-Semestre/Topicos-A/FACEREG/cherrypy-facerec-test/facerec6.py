670dcc6bcf5fb8a07d2c9297b0c12bf3b6fc8686
HandreMelo <34012482+HandreMelo@users.noreply.github.com>
2020-04-24 18:27:45 -0300

    Update facerec6.py
    >Carregamento e escrita em arquivo com atomismo para não dar problema caso seja fechado enquanto usando.
    >Novo método do pickle para reduzir código

--- IX-Semestre/Topicos-A/FACEREG/cherrypy-facerec-test/facerec6.py
+++ IX-Semestre/Topicos-A/FACEREG/cherrypy-facerec-test/facerec6.py
@@ -1,41 +1,55 @@
 import face_recognition
 import numpy as np
-import pickle
-
+from dataIO import pk as pickle
+import time
+#https://pythonhosted.org/dataIO/pk.html
 def cadastrar(file,name):
     all_face_encodings = {}
+    pickle_file = 'dataset_faces.pickle'
     load_image = face_recognition.load_image_file(file)
     image_encoding = face_recognition.face_encodings(load_image)[0]
-    all_face_encodings[name] = image_encoding
     try:
-        with open('dataset_faces.dat', 'wb') as f:
-            pickle.dump(all_face_encodings, f)
+        all_face_encodings = pickle.load(pickle_file)          
     except:
+        print("Arquivo vazio")
+        print("Depois : "+str(all_face_encodings))
+    try:
+        all_face_encodings[name] = image_encoding
+        pickle.safe_dump(all_face_encodings, pickle_file)
+    except:
+        raise
         return "Ocorreu um erro ao salvar os dados"
     
     return "Foto cadastrada com sucesso"
 
 def procurar(file):
     # Load face encodings
+    all_face_encodings = {}
+    pickle_file = 'dataset_faces.pickle'
+    erro = ""
     try:
-        with open('dataset_faces.dat', 'rb') as f:
-            all_face_encodings = pickle.load(f)
+        all_face_encodings = pickle.load(pickle_file)
+        if all_face_encodings:
+            print(all_face_encodings.keys())
+        else:
+            erro = "Ninguem Cadastrado"
     except:
-        return "Ocorreu um erro ao carregar os dados"
+        erro = "Não foi possivel acessar o banco de dados"
+        return erro
 
-    # Grab the list of names and the list of encodings
+    # Carregar a lista dos rostos codificados
     face_names = list(all_face_encodings.keys())
+    print(face_names)
     face_encodings = np.array(list(all_face_encodings.values()))
 
-    # Try comparing an unknown image
+    # Comparar imagem
     unknown_image = face_recognition.load_image_file(file)
     unknown_face = face_recognition.face_encodings(unknown_image)
-    count=-1
-    for res in face_recognition.compare_faces(face_encodings, unknown_face):
-        count+=1
-        if res==True:
-            print(face_names[count])
-            return face_names[count]
-        else:
-            print("NADA")
-            return "Não cadastrado"
+    result = face_recognition.compare_faces(face_encodings, unknown_face)
+
+    # Mostrar se e quem achou
+    names_with_result = dict(zip(result, face_names))
+    if True in names_with_result.keys():
+        return names_with_result[True]
+    return "Não encontrou ninguém"
+ 
